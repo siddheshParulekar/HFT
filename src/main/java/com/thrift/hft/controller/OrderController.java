3fdb2e325f3931ac8ca0276b22261eeac7cf7ccf
Siddhesh P <siddhesh.parulekar@neosoftmail.com>
2024-08-09 16:55:48 +0530

    razor pay

---
+++ src/main/java/com/thrift/hft/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.thrift.hft.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/order")
+public class OrderController {
+
+    private static final Logger logger = LogManager.getLogger(OrderController.class);
+
+
+
+}
